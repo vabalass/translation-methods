e595b6bd52fe5a74929e7d753012e6848ece42f0
vabalass <vabalas.balys@gmail.com>
2025-10-08 17:46:23 +0300

    changed to C parser

---
+++ sample.c
@@ -0,0 +1,12 @@
+int main() {
+    const int multiplier = 7;
+
+    int two = multiplier * 2;
+    int twenty_two = two + 6 + 2;
+
+    printf("number: %d", twenty_two);
+    
+    return 0;
+}
+
+/* Grąžina: number: 22 */
